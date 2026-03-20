0d9e6e214b62756abc947436a3145f46eb78331d
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-03-20 21:14:32 +0200

    removed old filenames and running it for the entire json

--- main.py
+++ main.py
@@ -22,27 +22,6 @@ from string import Template # fuck me
 
 # have some work todo: https://icalendar.org/validator.html.html?url=https://tzevaadom-ics.pages.dev/Test_2_CF.ics#results
 
-# python.exe" "main.py"
-
-# from pytz import timezone    
-
-
-    # def is_dst(zonename):
-    #     """Check if it is currently DST in a given timezone."""
-    #     tz = pytz.timezone(zonename)
-    #     # Get the current UTC time and make it aware
-    #     now_utc = pytz.utc.localize(datetime.utcnow())
-    #     # Convert to the target timezone and check the dst() offset
-    #     return now_utc.astimezone(tz).dst() != timedelta(0)
-
-    # # Usage examples (results depend on the current date and time)
-    # print(f"Is DST active in Los Angeles? {is_dst('America/Los_Angeles')}")
-    # print(f"Is DST active in London? {is_dst('Europe/London')}")
-
-    # for JsonObject in range(0, RawJsonLen):
-    #     print(JsonName[JsonObject][3], datetime.fromtimestamp(JsonName[JsonObject][3])) # the unix time is local and not UTC
-
-
 def FolderManagement():
     script_directory = Path(os.path.dirname(os.path.abspath(__file__)))
     LocalProdFolder = "Prod"
@@ -89,7 +68,6 @@ END:VCALENDAR"""
     IcsFinal += IcsHeader
     # for JsonObject in range(0, RawJsonLen):
     for JsonObject in range(0, 10):
-        # print(JsonName[JsonObject][3], datetime.fromtimestamp(JsonName[JsonObject][3]))
         EventTitle = "Red Alert in "
         for Place in JsonName[JsonObject][2]:
             EventTitle += Place
@@ -125,7 +103,6 @@ def WriteIcsToFile(IcsContent, Path, Filename):
     with open (Path / Filename, "w", encoding="utf-8", newline='\r\n') as IcsOnDisk:
         IcsOnDisk.write(IcsContent)
 
-
     FilePath = ""  
     return(FilePath)
 
@@ -143,7 +120,7 @@ GlobalTx = LocalTime(GlobalProdFolder)
 GlobalJsonFile = DownloadJsonDict(SourceJson, GlobalProdFolder, "Test")
 IcsContent = JsonIntoIcs(GlobalJsonFile)
 WriteIcsToFile(IcsContent, GlobalProdFolder, "Test_2_CF.ics")
-GitAddGitPush(SourceJson, """https://tzevaadom-ics.pages.dev/Test_2_CF.ics""")
+# GitAddGitPush(SourceJson, """https://tzevaadom-ics.pages.dev/Test_2_CF.ics""") # need to add check if ics os over 100mb
 
 
 
