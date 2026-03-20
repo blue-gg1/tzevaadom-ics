62716f90a272f6d71c69084001157999f387d015
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-03-20 18:23:24 +0200

    renamed the ICS file for CF to see if it will upload

--- main.py
+++ main.py
@@ -124,7 +124,7 @@ GlobalProdFolder = FolderManagement()
 GlobalTx = LocalTime(GlobalProdFolder)
 GlobalJsonFile = DownloadJsonDict(SourceJson, GlobalProdFolder, "Test")
 IcsContent = JsonIntoIcs(GlobalJsonFile)
-WriteIcsToFile(IcsContent, GlobalProdFolder, "Test1CF.ics")
+WriteIcsToFile(IcsContent, GlobalProdFolder, "Test_2_CF.ics")
 
 
 
