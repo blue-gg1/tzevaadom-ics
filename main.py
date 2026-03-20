9aba3c660eca6f0c16cd903eb3de571e465eac77
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-03-20 09:25:00 +0200

    starting wit a basic skeleton of code

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-import os
+import os, requests
+from pathlib import Path
 from settings import SourceJson
+from datetime import 
 
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
-print(script_directory)
+def FolderManagement():
+    script_directory = Path(os.path.dirname(os.path.abspath(__file__)))
+    LocalProdFolder = "Prod"
+    GlobalPodFolder = script_directory / LocalProdFolder
+    
+    print(script_directory.parent)
+    if os.path.exists(GlobalPodFolder) == False:
+        print("Folder is not real. Making now.")
+        os.mkdir(GlobalPodFolder)
+    else:
+        print("Folder is real.")
+    return GlobalPodFolder
 
-print(SourceJson)
+
+def DownloadJson(JsonUrl, DownloadPath):
+    rGetJson = requests.get(JsonUrl,
+                          )
+    if rGetJson.status_code != 200:
+        print("Failed")
+    else:
+        # print(rGetJson.content)
+        print("Success")
+
+FolderManagement()
+DownloadJson(SourceJson)
