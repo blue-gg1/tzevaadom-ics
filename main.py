34be818a615a593f8ed92c5d30efac20ab0158a2
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-03-20 20:21:31 +0200

    added comments to the code about the file.

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ from datetime import datetime, date, timezone, tzinfo
 from string import Template # fuck me
 
 
+# it takes this long to run and the file is 105 MB (110,669,824 bytes) big
+# Days              : 0
+# Hours             : 0
+# Minutes           : 11
+# Seconds           : 34
+# Milliseconds      : 638
+# Ticks             : 6946384871
+# TotalDays         : 0.00803979730439815
+# TotalHours        : 0.192955135305556
+# TotalMinutes      : 11.5773081183333
+# TotalSeconds      : 694.6384871
+# TotalMilliseconds : 694638.4871
+
 # python.exe" "main.py"
 
 # from pytz import timezone    
